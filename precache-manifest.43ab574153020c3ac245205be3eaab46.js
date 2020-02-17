7c932f2b7a559b8855fa87746bfd3119b71655a1
G4br13l4 <gabolf93@gmail.com>
2020-02-17 10:03:14 -0600

    Updates

---
+++ precache-manifest.43ab574153020c3ac245205be3eaab46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e002f9d3dfb38fc006847694959aa3f",
+    "url": "/silvia-peligro/index.html"
+  },
+  {
+    "revision": "7a4a4c0b43afdbd8550c",
+    "url": "/silvia-peligro/static/css/main.4db279a7.chunk.css"
+  },
+  {
+    "revision": "4c487ade0371ae02c48e",
+    "url": "/silvia-peligro/static/js/2.5d40cfb4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/silvia-peligro/static/js/2.5d40cfb4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a4a4c0b43afdbd8550c",
+    "url": "/silvia-peligro/static/js/main.97140394.chunk.js"
+  },
+  {
+    "revision": "a0688ba340e2d5c2bd7f",
+    "url": "/silvia-peligro/static/js/runtime-main.1ff5d4e9.js"
+  },
+  {
+    "revision": "d88e1ac5f582aa399c48281ea8405664",
+    "url": "/silvia-peligro/static/media/logo.d88e1ac5.jpg"
+  }
+]);
