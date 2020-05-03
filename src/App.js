301a64514900d66c2810aa745c83aed4e26dfa19
G4br13l4 <gabolf93@gmail.com>
2020-05-03 18:53:30 -0500

    Add mainbanner styles for mobile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="content-wrap">
       <FixedNavbar/>
-      {/* <MainBanner/> */}
+      <MainBanner/>
     </div>
       
   );
--- src/components/home/MainBanner.jsx
+++ src/components/home/MainBanner.jsx
@@ -3,6 +3,7 @@ import '../../styles/home/MainBanner.scss';
 import mainSlideBanner_1 from './../../assets/imgs/mainSlideBanner_1.jpg';
 import mainSlideBanner_2 from './../../assets/imgs/mainSlideBanner_2.jpg';
 import mainSlideBanner_3 from './../../assets/imgs/mainSlideBanner_3.jpg';
+import rhombusIcon from './../../assets/icons/rhombus-icon.svg';
 
 class MainBanner extends React.Component {
     constructor(props) {
@@ -14,14 +15,24 @@ class MainBanner extends React.Component {
     render() {
         return (
             <div className="mainSlideBanner">
-                <div className="mainSlideBanner__boxImg"> 
+                <div style={{backgroundImage: `url(${mainSlideBanner_1})` }} className="mainSlideBanner__boxImg"> 
                 </div>
-                
-                <div className="mainSlideBanner__boxText">
+                <div className="mainSlideBanner__txtBox">
                     <h1 className="title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
                     <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque finibus lacus sit amet auctor. Proin et purus nulla. </p>
                     <div>
-                        <button>Ver más</button>
+                        <button className="see-more-btn"><span>Ver más</span></button>
+                    </div>
+                </div>
+                <div className="mainSlideBanner__icons">
+                    <div className="rhombus">
+                        <img src={rhombusIcon}/>
+                        <img src={rhombusIcon}/>
+                        <img src={rhombusIcon}/>
+                    </div>
+                    <div className="arrows">
+                        <i class="material-icons">navigate_before</i>
+                        <i class="material-icons">navigate_next</i>
                     </div>
                 </div>
             </div>
