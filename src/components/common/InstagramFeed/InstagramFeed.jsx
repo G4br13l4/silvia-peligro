cfa9382c0993e4736b909fb721f07e7d4dbc8c91
G4br13l4 <gabolf93@gmail.com>
2020-08-09 18:43:00 -0500

    Add tester Instagram Feed

--- src/components/common/InstagramFeed/InstagramFeed.jsx
+++ src/components/common/InstagramFeed/InstagramFeed.jsx
@@ -1,39 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './InstagramFeed.scss';
 import axios from 'axios';
-import { dummyArray } from './dummyArray';
 
 export const InstagramFeed = props => {
     const { postsToLoad } = props;
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        // axios.get(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&access_token=IGQVJXQzF0ektpLTQxdURjVVI1aWxmNE5lc1I3NEEtVGo3RG5ub3JtSlRwMnJITEFaekpPN2dUWU53b3hsWGNMZA2pTVUx5TXoxUldhaVE0Q0QxSFZADVnoyZA0FjVVFJam9WZAmVfdU1yUDBjX2RKRXNDRAZDZD`)
-        // .then(res => {
-        //     console.log(res.data.data);
-        //     setPosts(res.data.data);
+        const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+        axios.get(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&access_token=${API_TOKEN}&limit=10`)
+        .then(res => {
+            setPosts(res.data.data);
 
-        // })
-        console.log(dummyArray)
-        setPosts(dummyArray);
+        })
     }, [postsToLoad]);
 
     return (
-        <div className="instagramFeed">
-            { posts.map(p => {
-                return(
-                 p.media_type === 'VIDEO' ?
-                    <div key={p.id} className="post">
-                        <video controls preload="none" poster={p.thumbnail_url}>
-                            <source src={p.media_url} type="video/mp4"/>
-                        </video>
-                    </div>
-                    : 
-                    <div key={p.id} className="post">
-                        <img src={p.media_url} alt={p.caption} loading="lazy"/>
-                    </div>
-                );
-            })}
-        </div>
+        <>
+            <h2 className="follow-me">Sígueme en Instagram</h2>
+            <div className="follow-me">@speligro</div>
+            <div className="instagramFeed">
+                { posts.map(p => {
+                    return(
+                    p.media_type === 'VIDEO' ?
+                        <div key={p.id} className="post" style={{backgroundImage: `url(${p.thumbnail_url})`}}/>
+                        : 
+                        <div key={p.id} className="post" style={{backgroundImage: `url(${p.media_url})`}}/>
+                    );
+                })}
+            </div>  
+        </>
     );
 }
