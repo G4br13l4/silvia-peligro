a7f6005ee93d8a80e03b16bf50a2b66b5ebffd7b
G4br13l4 <gabolf93@gmail.com>
2020-09-21 15:05:27 -0500

    Add table to Kits Page

--- src/components/Products/KitsPage/KitsPage.js
+++ src/components/Products/KitsPage/KitsPage.js
@@ -12,6 +12,42 @@ const KitsPage = props => {
             <h1>Kits de Bordado</h1>
             <span>Descubre todos los diseños para que te diviertas bordando.</span>
           </header>
+          <div className="kits-section__content">
+            <p>
+              Estos kits son ideales para bordadores experimentados, por lo que si es tu primera vez bordando,  te recomendamos adquirir uno de nuestros cursos en línea sobre bordado básico ó asistir a uno de nuestros talleres.
+            </p>
+            <h2>Tipos de Kits</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>¿Qué incluye?</th> 
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="table-row">
+                  <td className="title small"><span>Chico</span></td>
+                  <td className="content">Lorem ipsum dolor sit amet, consectetur.
+                  Pellentesque interdum massa in dui aliquam.
+                  Sed tincidunt velit in massa fermentum.
+                  Ut id metus eget quam.
+                  Morbi ut risus ut purus scelerisque.
+                  Checa los modelos disponibles en xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                  </td>
+                </tr>
+                <tr className="table-row">
+                  <td className="title large"><span>Grande</span></td>
+                  <td className="content">Lorem ipsum dolor sit amet, consectetur.
+                  Pellentesque interdum massa in dui aliquam.
+                  Sed tincidunt velit in massa fermentum.
+                  Ut id metus eget quam.
+                  Morbi ut risus ut purus scelerisque.
+                  Checa los modelos disponibles en xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         <Footer />
       </section>
     );
