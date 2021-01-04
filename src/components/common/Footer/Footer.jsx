fd32c384041d9c0f040a25a28bb76a5d5ea53323
G4br13l4 <gabolf93@gmail.com>
2021-01-03 22:47:34 -0600

    Add subscribe mailing section

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -19,11 +19,41 @@ const Footer = props => {
         <footer className="footer">
             <div className="footer__border top-border"><span>------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</span></div>
             <div className="footer__input-wrapper">
-                <p>{t('Mailing.title')}</p>
-                <InputGroup className="custom-input">
-                    <Input placeholder={t('Mailing.input')}/>
+
+                {/* <InputGroup className="custom-input">
+                    <Input placeholder={t('Mailing.input')} required/>
                     <InputGroupAddon addonType="prepend"><Button>{t('Mailing.btn')}</Button></InputGroupAddon>
-                </InputGroup>
+                </InputGroup> */}
+
+                <p className="intro">{t('Mailing.title')}</p>
+                {/* Begin Mailchimp Signup Form */}
+                <div id="mc_embed_signup">
+                    <form action="https://kichink.us8.list-manage.com/subscribe/post?u=b9cb0284d7852b9c555efb828&amp;id=434325209d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
+                        <div id="mc_embed_signup_scroll">
+                            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                            <div className="mc-field-group">
+                                <label for="mce-EMAIL">{t('Mailing.email')}<span class="asterisk">*</span>:</label>
+                                <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required/>
+                            </div>
+                            <div className="mc-field-group">
+                                <label for="mce-FNAME">{t('Mailing.firstName')}: </label>
+                                <input type="text" name="FNAME" className="name" placeholder={`${t('Input.optional')}`} id="mce-FNAME"/>
+                            </div>
+                            <div id="mce-responses" className="clear">
+                                <div className="response" id="mce-error-response" style={{display:'none'}}></div>
+                                <div className="response" id="mce-success-response" style={{display:'none'}}></div>
+                            </div>   
+                            <div style={{position:'absolute',left:'-5000px'}} aria-hidden="true">
+                                <input type="text" name="b_b9cb0284d7852b9c555efb828_434325209d" tabindex="-1"/>
+                            </div>
+                            <div className="clear">
+                                <input type="submit" value={t('Button.subscribe')} name="subscribe" id="mc-embedded-subscribe" className="button"/>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                {/* End mc_embed_signup */}
+
             </div>
             <div className="footer__border middle-border"><span>***************************************************************************************************************************************************************************************************************************************</span></div>
             <div className="footer__menu">
